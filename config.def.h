fbdcab1b42976193b2548a5a7d0e693a38534e43
Marian Minar <majamin@gmail.com>
2020-12-19 08:45:20 -0800

    change scroll keybind

--- config.def.h
+++ config.def.h
@@ -204,8 +204,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_K,           kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_J,           kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_K,           kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_J,           kscrolldown,    {.i = -1} },
 };
 
 /*
