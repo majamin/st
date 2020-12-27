2d67bcb0f49aba41fb077b8659999421bd794a06
Marian Minar <majamin@gmail.com>
2020-12-27 13:59:40 -0800

    add alternate font patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "mono:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = {
+	"JoyPixels:pixelsize=18:antialias=true:autohint=true",
+	"Symbols Nerd Font:pixelsize=18:antialias=true:autohint=true",
+	/*add more here*/
+};
 static int borderpx = 2;
 
 /*
