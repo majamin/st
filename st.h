c0c5048296780b3dbe8d3bbb85552eaf073a3901
Marian Minar <majamin@gmail.com>
2021-12-25 15:14:06 -0800

    add undercurl capability

--- st.h
+++ st.h
@@ -37,6 +37,7 @@ enum glyph_attribute {
 	ATTR_LIGA       = 1 << 11,
 	ATTR_SELECTED   = 1 << 12,
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
+	ATTR_DIRTYUNDERLINE = 1 << 15,
 };
 
 enum selection_mode {
@@ -68,6 +69,8 @@ typedef struct {
 	ushort mode;      /* attribute flags */
 	uint32_t fg;      /* foreground  */
 	uint32_t bg;      /* background  */
+	int ustyle;	  /* underline style */
+	int ucolor[3];    /* underline color */
 } Glyph;
 
 typedef Glyph *Line;
