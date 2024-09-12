ebe8ba49dd6919454b9fe90d855a8f3ee8f253ae
Marian Minar <majamin@gmail.com>
2024-09-11 19:57:00 -0700

    jetbrains font good

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Nerd Font:pixelsize=22:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:style:medium:size=12";
 static int borderpx = 24;
 
 /*
